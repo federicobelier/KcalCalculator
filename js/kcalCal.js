056b133afd17df260656566d172b77303046622c
FedericoB <federico.codedev@gmail.com>
2023-01-23 15:58:05 -0500

    vairables creadas. Pendiente: funciones y logica matematica

---
+++ js/kcalCal.js
@@ -0,0 +1,28 @@
+//Variables globales
+let name = prompt("Ingresa tu Nombre");
+let age = prompt("Ingresa tu edad");
+let gender = prompt ("Selecciona tu genero")
+let weight = prompt("Ingresa tu peso en KG");
+let height = prompt("Ingresa tu estatura en CM");
+let physicAct = prompt("Cuanto te ejercitas? Seleccion de la siguientes opcione");
+let physicActKcal = prompt ("En Calorias, cuanto es tu gasto energetico?")
+
+//Variables locales
+let bmrMen = (9,99 * weight) + (6,25 * height) - (492 * age) + 5;
+let bmrWomen = (9,99 * weight) + (6,25 * height) - (492 * age) - 161;
+
+let bmrXpa = ;
+let bmrXpakcal = ;
+
+//Gasto Energetico
+switch (physicAct) {
+    case :
+        
+        break;
+
+    default:
+        break;
+}
+
+
+console.log(nombre +"Necesitas consumir " + kcalIntake + " kCals el dia de hoy. ")
