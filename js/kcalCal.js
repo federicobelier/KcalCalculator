40f4a5d89e8419fe31aa6b2156892f87a83bdc80
FedericoB <federico.codedev@gmail.com>
2023-01-30 19:55:45 -0500

    bmrCalculation() completed, paCalculation() needs to store  bmrCalculation result in a variable no get the result of paCalculation()

--- js/kcalCal.js
+++ js/kcalCal.js
@@ -7,54 +7,53 @@ let gender = prompt ("Selecciona tu genero: Masculino o Femenino").toLowerCase()
 
 function bmrCalculation(){
     if(gender =="masculino"){
-        let bmrMen = (9,99 * weight) + (6,25 * height) - (492 * age) + 5;
-        console.log(bmrMen);
-    } else if (gender != "masculino"){
-        let bmrWomen = (9,99 * weight) + (6,25 * height) - (492 * age) - 161;
-        console.log(bmrWomen);
-    } 
+        const bmrMen = Math.round ((9.99 * weight) + (6.25 * height) - (4.92 * age) + 5);
+        alert(name + ", tu tasa metabolica basal es de "+ bmrMen + " kCals");
+    } else if (gender != "masculino") {
+        const bmrWomen = Math.round ((9.99 * weight) + (6.25 * height) - (4.92 * age) - 161);
+        alert(name + ", tu tasa metabolica basal es de "+ bmrWomen + " kCals");;
+    } else {
+        alert("Ingresar un genero")}
     }
-
-bmrCalculation();
-
-
-
 
 //Physic Activity Calculation
-/*
-let physicAct = prompt("Cuanto te ejercitas? Seleccion de la siguientes opciones").toLowerCase();
-let physicActKcal = prompt ("En Calorias, cuanto es tu gasto energetico? Ingresa valor en Kcal");
 
-const bmrXpa = bmr * physicAct;
-const bmrXpakcal = bmr + physicActKcal;
-
-while (physicAct!=""){
-    switch (physicAct) {
-        case "sedentario":
-            
-            break;
-        case "ligeramente activo":
-
-            break;
-        case "moderadamente activo":
-
-            break;
-        case "muy activo":
-
-            break;
-        case "extra activo":
-
-            break
+function paCalculation() {
+    let conteo = prompt("Cuentas tu gasto energetico? \n Si o No").toLowerCase();
+    if (conteo != "si") {
+        let physicAct = prompt("Cuanto te ejercitas? Seleccion de la siguientes opciones:\n - Sedentario \n - Ligeramente Activo \n - Moderradamente Activo \n - Muy Activoo \n - Extra Activo").toLowerCase();
+        switch (physicAct) {
+            case "sedentario":
+                physicAct = 1.2;
+                break;
+            case "ligeramente activo":
+                physicAct = 1.375;
+                break;
+            case "moderadamente activo":
+                physicAct = 1.55;
+                break;
+            case "muy activo":
+                physicAct = 1.725;
+                break;
+            case "extra activo":
+                physicAct = 1.9
+                break
+            default:
+                alert("Ingresar una opcion")
+                break;
+        }
+        let resultadoUno = bmr + physicAct;
+        alert("En base a tu actividad fisica necesitas un consumo de " + resultadoUno + " kCals para mantenerte con energia.")
     
-        default:
-            alert("Ingresar una opcion")
-            break;
+    } else if(conteo == "si"){
+        let physicActKcal = prompt ("En Calorias, cuanto es tu gasto energetico? Ingresa valor en Kcal");
+        let calculoDos = bmr + physicActKcal;
+        alert("en base a tu actividad fisica necesitas un consumo de " + calculoDos + " kCals para mantenerte con energia.")
+    } else {
+        alert("Ingresa una opcion")
     }
 }
-
-
-
-
-//let result = console.log(nombre +"Necesitas consumir " + bmrXpa + " kCals el dia de hoy. ")
-//let result = console.log(nombre +"Necesitas consumir " + bmrXpakcal + " kCals el dia de hoy. ")
-*/
+ 
+bmrCalculation();
+const bmr = parseInt(bmrCalculation)
+paCalculation();
